e198af076f20cd6e543dbf1016e1a8ea4f8c2c4e
Clément Doumouro <clement.doumouro@gmail.com>
2026-04-17 12:21:40 +0200

    fix: errors after rebase

--- scripts/lock-dist.sh
+++ scripts/lock-dist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-cd "$1"
+cd workers/"$1"
 # uv bug -n flag to discard cache takes 2 times to work
 uv lock -n --no-sources-package datashare-python || uv lock -n --no-sources-package datashare-python
 cp uv.lock uv.dist.lock
