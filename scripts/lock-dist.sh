25a553b19de0241059defa60962ac180c52d2797
Clément Doumouro <clement.doumouro@gmail.com>
2026-04-17 13:20:07 +0200

    chore: bump to `datashare-python==0.6.0`

--- scripts/lock-dist.sh
+++ scripts/lock-dist.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-cd workers/"$1"
+if [[ "$1" == "worker-template" ]];then
+    cd "$1"
+else
+    cd workers/"$1"
+fi
+
 # uv bug -n flag to discard cache takes 2 times to work
 uv lock -n --no-sources-package datashare-python || uv lock -n --no-sources-package datashare-python
 cp uv.lock uv.dist.lock
