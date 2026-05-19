35ae3449a6c920be74031d8b92e83dc085c1c065
Clément Doumouro <clement.doumouro@gmail.com>
2026-05-19 15:11:12 +0200

    chore: fix datashare python versioning deadlock in workflow worker

--- workers/workflows-worker/bump_workflow.sh
+++ workers/workflows-worker/bump_workflow.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
+worker_lib="$1"
 
-uv add -n "$1==$2" || uv add -n "$1==$2"
+if [[ "$worker_lib" == "datashare-python" ]]; then
+    op="~="
+else
+    op="=="
+fi
+
+uv add -n "$1$op$2" || uv add -n "$1$op$2"
 git add pyproject.toml uv.lock
 git commit -m "chore(workflows-worker): bump $1 to $2"
