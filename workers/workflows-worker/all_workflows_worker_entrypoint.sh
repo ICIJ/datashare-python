65b7e0ad7cd2a701269113a45c78c796388c4dff
Clément Doumouro <clement.doumouro@gmail.com>
2026-05-20 12:15:53 +0200

    feature(workflows-worker): skip config discovery

--- workers/workflows-worker/all_workflows_worker_entrypoint.sh
+++ workers/workflows-worker/all_workflows_worker_entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 uv run --no-sync datashare-python worker start \
+    --skip-config-discovery \
     --queue datashare.workflows \
     --workflow asr.transcription \
     --workflow translation
