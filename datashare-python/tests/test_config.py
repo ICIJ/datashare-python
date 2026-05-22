ad06b698d30dd839d471f258637e94157b3271d4
Clément Doumouro <clement.doumouro@gmail.com>
2026-05-22 12:44:20 +0200

    feature(datashare-python): support exporting prometheus metrics

--- datashare-python/tests/test_config.py
+++ datashare-python/tests/test_config.py
@@ -1,6 +1,9 @@
 import os
+from unittest.mock import AsyncMock, patch
 
-from datashare_python.config import WorkerConfig
+from datashare_python.config import (
+    WorkerConfig,
+)
 
 
 def test_worker_config_loggers_from_env(reset_env) -> None:  # noqa: ANN001, ARG001
@@ -11,3 +14,17 @@ def test_worker_config_loggers_from_env(reset_env) -> None:  # noqa: ANN001, ARG
     config = WorkerConfig()
     # Then
     assert config.logging.loggers["datashare_python"] == "WARNING"
+
+
+async def test_worker_config_should_export_prometheus_metrics(reset_env) -> None:  # noqa: ANN001, ARG001
+    # Given
+    prometheus_host = "0.0.0.0:9000"
+    os.environ["DS_WORKER_TEMPORAL__PROMETHEUS_HOST"] = prometheus_host
+    config = WorkerConfig()
+    assert config.temporal.prometheus_host == prometheus_host
+    # When
+    mock_connect = AsyncMock()
+    with patch("datashare_python.config.TemporalClient.connect", mock_connect):
+        await config.temporal.to_client()
+    # Then
+    assert mock_connect.await_args_list[0].kwargs["runtime"] is not None
