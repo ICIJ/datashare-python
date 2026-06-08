6df98c60663a1ff96d37d7f40c48543e27d48bd2
Clément Doumouro <clement.doumouro@gmail.com>
2026-06-08 16:42:43 +0200

    fix(translation-worker): queue names in entrypoints

--- workers/translation-worker/entrypoints/inference_worker.sh
+++ workers/translation-worker/entrypoints/inference_worker.sh
@@ -3,4 +3,4 @@
 uv run --no-sync datashare-python worker start \
     --dependencies translation.inference \
     --queue translation.inference.cpu \
-    --activity translation.translate_docs
+    --activity translation.translate-docs
--- workers/translation-worker/entrypoints/io_worker.sh
+++ workers/translation-worker/entrypoints/io_worker.sh
@@ -4,5 +4,5 @@ set -e
 uv run --no-sync datashare-python worker start \
     --dependencies translation.io \
     --queue translation.io \
-    --activity translation.worker_config \
-    --activity translation.create_translation_batches
+    --activity translation.worker-config \
+    --activity translation.create-translation-batches
