4e2632a588e6621b9fdf8fa115dfd2fc9aa4b4ba
Clément Doumouro <clement.doumouro@gmail.com>
2026-06-09 10:10:40 +0200

    fix(workflows-worker): add missing extract workflow

--- workers/workflows-worker/all_workflows_worker_entrypoint.sh
+++ workers/workflows-worker/all_workflows_worker_entrypoint.sh
@@ -5,4 +5,5 @@ uv run --no-sync datashare-python worker start \
     --skip-config-discovery \
     --queue datashare.workflows \
     --workflow asr.transcription \
-    --workflow translation
+    --workflow translation \
+    --workflow extract.markdown
