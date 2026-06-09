0a15236638a67d729c8a5130df42d6925a3d2c9f
Clément Doumouro <clement.doumouro@gmail.com>
2026-06-09 10:10:40 +0200

    fix(workflows-worker): add missing extract workflow

--- workers/workflows-worker/all_workflows_worker_entrypoint.sh
+++ workers/workflows-worker/all_workflows_worker_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+# TODO: use regex to automatically add new workflows from the same app
 uv run --no-sync datashare-python worker start \
     --skip-config-discovery \
     --queue datashare.workflows \
