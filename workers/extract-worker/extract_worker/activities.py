8eec1c1af6bf10a92341932d83020ddc89d98fc8
Clément Doumouro <clement.doumouro@gmail.com>
2026-06-17 12:04:19 +0200

    chore(extract-worker): increase heartbeat timeout for processing

--- workers/extract-worker/extract_worker/activities.py
+++ workers/extract-worker/extract_worker/activities.py
@@ -10,6 +10,7 @@ from typing import Any, cast
 
 from datashare_python.dependencies import lifespan_es_client, lifespan_worker_config
 from datashare_python.objects import DocArtifact, Document, DocumentLocation
+from datashare_python.types_ import ProgressRateHandler
 from datashare_python.utils import (
     ActivityWithProgress,
     activity_defn,
@@ -37,7 +38,6 @@ from icij_common.es import (
     has_type,
 )
 from pydantic import TypeAdapter
-from types_ import ProgressRateHandler
 
 from .config import ExtractWorkerConfig
 from .constants import MARKDOWN_DIRNAME, MARKDOWN_METADATA_KEY
